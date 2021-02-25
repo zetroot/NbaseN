7b730c4ba27c8a66303da2b3289c9a51031dc5e6
zetroot <zetroot@gmail.com>
2021-02-25 11:23:07 +0300

    added negative numbers support #4

--- tests/NbaseN.Tests/Int32ConversionE2ETests.cs
+++ tests/NbaseN.Tests/Int32ConversionE2ETests.cs
@@ -7,9 +7,13 @@ namespace NbaseN.Tests
         [Theory]
         [InlineData(0, "0")]
         [InlineData(1, "1")]
+        [InlineData(-1, "-1")]
         [InlineData(2, "10")]
+        [InlineData(-2, "-10")]
         [InlineData(100500, "11000100010010100")]
+        [InlineData(-100500, "-11000100010010100")]
         [InlineData(2147483647, "1111111111111111111111111111111")]
+        [InlineData(-2147483647, "-1111111111111111111111111111111")]
         public void ConvertToString_WhenCalledWithBase2_CovertsCorrect(int decInput, string binExpected)
         {
             // act
@@ -22,11 +26,17 @@ namespace NbaseN.Tests
         [Theory]
         [InlineData(0, "0")]
         [InlineData(1, "1")]
+        [InlineData(-1, "-1")]
         [InlineData(2, "2")]
+        [InlineData(-2, "-2")]
         [InlineData(10, "A")]
+        [InlineData(-10, "-A")]
         [InlineData(16, "10")]
+        [InlineData(-16, "-10")]
         [InlineData(100500, "18894")]
+        [InlineData(-100500, "-18894")]
         [InlineData(2147483647, "7FFFFFFF")]
+        [InlineData(-2147483647, "-7FFFFFFF")]
         public void ConvertToString_WhenCalledWithBase16_CovertsCorrect(int decInput, string binExpected)
         {
             // act
@@ -39,10 +49,15 @@ namespace NbaseN.Tests
         [Theory]
         [InlineData(0, "0")]
         [InlineData(1, "1")]
+        [InlineData(-1, "-1")]
         [InlineData(2, "2")]
+        [InlineData(-2, "-2")]
         [InlineData(10, "A")]
+        [InlineData(-10, "-A")]
         [InlineData(16, "G")]
+        [InlineData(-16, "-G")]
         [InlineData(17325410, "ABCDE")]
+        [InlineData(-17325410, "-ABCDE")]
         public void ConvertToString_WhenCalledWithBase36_CovertsCorrect(int decInput, string binExpected)
         {
             // act
